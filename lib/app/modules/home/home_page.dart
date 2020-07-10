4336d2969f8cea2e85add7e6de017dbadf57ca9a
Railton98 <railtonleal98@gmail.com>
2020-07-10 02:35:47 -0300

    home module initialized

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -17,10 +15,24 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Play List'),
+        backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: Colors.black,
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, index) {
+          return ListTile(
+            leading: Image.network(
+                'https://images-na.ssl-images-amazon.com/images/I/81bqaMrqceL._AC_SX425_.jpg'),
+            title: Text('Californication', style: GoogleFonts.ptSansCaption()),
+            subtitle: Text(
+              'Red Hot Chilli Peppers',
+              style: GoogleFonts.ptSansCaption(),
+            ),
+            contentPadding: EdgeInsets.all(10),
+          );
+        },
       ),
     );
   }
